54b262a26a6bedd23addbe693c779190ce65af27
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-17 23:16:10 +0200

    Modify Info.tsx

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -3,11 +3,7 @@ interface InfoProps {
 }
 
 const Info = ({ number }: InfoProps): JSX.Element => {
-  return (
-    <section className="controls">
-      <p className="info">{number} gentlemen pointing at you</p>
-    </section>
-  );
+  return <p className="info">{number} gentlemen pointing at you</p>;
 };
 
 export default Info;
